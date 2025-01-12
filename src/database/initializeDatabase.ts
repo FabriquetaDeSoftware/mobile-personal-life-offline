6d7df2eea8ed80a8b6b48ad34df47cd9c88d4377
cezarfbc <cezarfbc@gmail.com>
2025-01-12 19:11:29 -0300

    feat: initialize database with todoList and financeAccount tables; add constraints for data integrity

--- src/database/initializeDatabase.ts
+++ src/database/initializeDatabase.ts
@@ -2,16 +2,30 @@ import { type SQLiteDatabase } from 'expo-sqlite';
 
 export async function initializeDatabase(database: SQLiteDatabase) {
   await database.execAsync(/* sql */ `
+    -- Tabela de tarefas
     CREATE TABLE IF NOT EXISTS todoList (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         content TEXT NOT NULL,
         status TEXT NOT NULL CHECK(status IN ('pending', 'completed'))
     );
 
+    -- Tabela de contas
     CREATE TABLE IF NOT EXISTS financeAccount (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT NOT NULL,
-        balance REAL NOT NULL
+        balance REAL NOT NULL CHECK(balance >= 0)
     );
+
+    -- -- Tabela de despesas
+    -- CREATE TABLE IF NOT EXISTS expense (
+    --     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    --     accountId INTEGER NOT NULL,
+    --     description TEXT NOT NULL,
+    --     createdAt TEXT NOT NULL, 
+    --     type TEXT NOT NULL CHECK(type IN ('income', 'expense')),
+    --     name TEXT NOT NULL,
+    --     status TEXT NOT NULL CHECK(status IN ('pending', 'completed')), 
+    --     FOREIGN KEY (accountId) REFERENCES account (id) ON DELETE CASCADE
+    -- );
 `);
 }
